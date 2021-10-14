ddb0c57f3eee7434ca86ebba3eb036e9ce18da18
Tamii-h <tamii4riadnahernandez@gmail.com>
2021-10-13 21:28:45 -0300

    Ejercicio 4

---
+++ Punto_4.py
@@ -0,0 +1,15 @@
+#Genere un ejemplo del bucle "for" en el cual recorra los elementos de una lista y los imprima por consola.
+
+lista = [1, 2, 3, "HOLAA MUNDOO"]
+
+for i in lista:
+     print(i)
+    
+print("------------------------------------ \n ---------------------------------")
+
+# Genere un ejemplo del bucle "while" en el cual se imprima 5 veces la siguiente frase por consola: "La condición se cumplio."
+
+contador = 0
+while contador < 5:
+    print("La condición se cumplio.")
+    contador = contador + 1
