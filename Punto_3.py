8e82b62b300a9ab22e39cc8690c4a8ea9b5acd9b
Tamii-h <tamii4riadnahernandez@gmail.com>
2021-10-13 21:28:30 -0300

    Ejercicio 3

---
+++ Punto_3.py
@@ -0,0 +1,37 @@
+#Inicio de mi programa
+            ###### Codigo original ######
+
+mi_variable = 4
+
+if mi_variable == 0:
+    print("Ha ingresado a la primera validación.")
+
+elif mi_variable > 3:
+    print("Ha ingresado a la segunda validación.")
+
+elif mi_variable > 6:
+    print("Ha ingresado a la tercer validación.")
+
+else: 
+    print("No hay coincidencias.")
+
+# Fin del código
+
+            ##### Codigo modificado #####
+            
+# Inicio de mi programa
+mi_variable = 4
+
+if mi_variable == 0:
+    print("Ha ingresado a la primera validación.")
+
+elif mi_variable > 6:
+    print("Ha ingresado a la segunda validación.")
+
+elif mi_variable > 3:
+    print("Ha ingresado a la tercer validación.")
+
+else: 
+    print("No hay coincidencias.")
+
+# Fin del código
